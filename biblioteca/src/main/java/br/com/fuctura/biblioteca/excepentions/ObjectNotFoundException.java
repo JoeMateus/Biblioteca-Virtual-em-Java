95c9956a5360004a638a39c787e733b930f669eb
Jonny Mateus Santana Silva <jonnymateusuni14@gmail.com>
2025-03-12 14:38:52 -0300

    Delete biblioteca directory

--- biblioteca/src/main/java/br/com/fuctura/biblioteca/excepentions/ObjectNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package br.com.fuctura.biblioteca.excepentions;
-
-    // frase de quando nao tiver mais categoria
-    public class ObjectNotFoundException extends RuntimeException{
-
-        public ObjectNotFoundException(String message) {
-            super (message);
-        }
-    }
